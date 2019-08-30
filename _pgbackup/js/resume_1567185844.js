3961892602209333c5dcb9bb98240b8788af8a57
MILIND PRAJAPATI <43792965+milindprajapati@users.noreply.github.com>
2019-08-30 13:55:11 -0400

    V7.0 Counter Finalised

--- _pgbackup/js/resume_1567185844.js
+++ _pgbackup/js/resume_1567185844.js
@@ -1,16 +1,3 @@
- function counter_fn(){    
-        var n = localStorage.getItem('on_load_counter');
-
-        if (n === null) {
-            n = 0;
-        }
-
-        n++;
-
-        localStorage.setItem("on_load_counter", n);
-
-        document.getElementById('counter').innerHTML = n;
-        }
 (function($) {
   "use strict"; // Start of use strict
 
